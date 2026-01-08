33309e514050d22b51733d9e1efc10b664d528f0
Pranjal Pandey <pranjal3210@gmail.com>
2026-01-08 14:27:23 +0530

    Completed practice classwork ques2

---
+++ practice_classwork/ques2.js
@@ -0,0 +1,30 @@
+function getUser(username) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: "Rahul",
+        type: "Premium"
+      });
+    }, 1500);
+  });
+}
+function checkSubscription(user) {
+  return new Promise((resolve, reject) => {
+    if (user.type === "Premium") {
+      resolve("Access Granted to Netflix");
+    } else {
+      reject("Please Subscribe");
+    }
+  });
+}
+async function authenticateUser(username) {
+  try {
+    const user = await getUser(username);          
+    const access = await checkSubscription(user);  
+    console.log(access);
+  } catch (error) {
+    console.log(error);
+  }
+}
+authenticateUser("rahul");
+authenticateUser("pranjal");
